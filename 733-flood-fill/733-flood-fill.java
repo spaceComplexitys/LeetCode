091799e67b17f112ea7000f8f0f4cdf7ac4a60c5
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-06-15 23:18:57 -0400

    Time: 0 ms (100.00%), Space: 43.3 MB (81.95%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.java
@@ -0,0 +1,16 @@
+class Solution {
+   public int[][] floodFill(int[][] a, int s, int si, int n) {
+    task(a,s,si,n,a[s][si]);
+    return a;
+}
+static void task(int[][] a, int s, int si, int n, int k)
+{
+    if(s<0||s>=a.length||si<0||si>=a[0].length||a[s][si]==n||a[s][si]!=k)
+        return;
+    a[s][si]=n;
+    task(a,s,si+1,n,k);
+    task(a,s,si-1,n,k);
+    task(a,s+1,si,n,k);
+    task(a,s-1,si,n,k);
+}
+}
