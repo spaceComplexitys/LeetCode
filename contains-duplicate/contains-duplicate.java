52fc50919bc3763f725bc85debd06aecd2e5e301
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-06-27 18:12:53 -0400

    Time: 12 ms, Memory: 69.9 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+class Solution {
+     public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
