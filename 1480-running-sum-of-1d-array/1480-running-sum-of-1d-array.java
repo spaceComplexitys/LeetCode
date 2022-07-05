3627c3857f05174d62189cfafff444daf477cef5
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-07-05 13:50:15 -0400

    Time: 0 ms (100.00%), Space: 43.5 MB (36.26%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,10 +1,21 @@
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+//         for (int i = 0; i < nums.length-1; i++) {
+//             int tempRunningSum = nums[i] + nums[i+1];
+//             nums[i+1] = tempRunningSum;
+//         }
+
+//         return nums;
+//     }
+// }
+
 class Solution {
     public int[] runningSum(int[] nums) {
-        for (int i = 0; i < nums.length-1; i++) {
-            int tempRunningSum = nums[i] + nums[i+1];
-            nums[i+1] = tempRunningSum;
-        }
-
+            int i = 1;
+            while (i<nums.length){
+                nums[i]+=nums[i-1];
+                i++;
+            }
         return nums;
     }
 }
