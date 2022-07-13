37e5be07e448a4bf95d1c9bbadf8533cde48268c
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-07-13 16:44:45 -0400

    Time: 1 ms (56.63%), Space: 45.5 MB (27.82%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        var slow = head;
+	var fast = head;
+
+	// If there's a cycle, let the pointers enter it and meet
+	while (fast != null && fast.next != null) {
+		slow = slow.next;
+		fast = fast.next.next;
+		if (slow == fast)
+			break;
+	}
+
+	// check if there's no cycle
+	if (fast == null || fast.next == null)
+		return null;
+
+	// find the entry point of the cycle
+	slow = head;
+	while (slow != fast) {
+		slow = slow.next;
+		fast = fast.next;
+	}
+	return slow;
+    }
+}
