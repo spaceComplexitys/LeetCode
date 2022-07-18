a47dd5b89a0f7102d2a94a378cdebd760a00662d
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-07-18 18:24:34 -0400

    Time: 0 ms, Memory: 42.4 MB - LeetHub

--- find-peak-element/find-peak-element.java
+++ find-peak-element/find-peak-element.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        int max = 0;
-        int maxIndex = 0;
-        
-        if (nums.length > 0) {
-            max = nums[0];
+      public int findPeakElement(int[] nums) {
+        if(nums.length == 1) return 0; // single element
+
+        int n = nums.length;
+
+        // check if 0th/n-1th index is the peak element
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+        // search in the remaining array
+        int start = 1;
+        int end = n-2;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] < nums[mid-1]) end = mid - 1;
+            else if(nums[mid] < nums[mid+1]) start = mid + 1;
         }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > max) {
-                maxIndex = i;
-                max = nums[i];
-            }
-        }
-        
-        return maxIndex;
+        return -1; // dummy return statement
     }
 }
