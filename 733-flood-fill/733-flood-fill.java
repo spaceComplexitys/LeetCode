65d67fd60ceac8ce8bb68edc56e983557ad35850
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-07-18 17:29:38 -0400

    Time: 0 ms (100.00%), Space: 43.1 MB (93.78%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -5,9 +5,12 @@ class Solution {
 }
 static void task(int[][] a, int s, int si, int n, int k)
 {
-    if(s<0||s>=a.length||si<0||si>=a[0].length||a[s][si]==n||a[s][si]!=k)
+    if(s < 0||s >= a.length 
+       || si < 0 || si >=a[0].length
+       ||a[s][si] == n 
+       ||a[s][si] != k)
         return;
-    a[s][si]=n;
+    a[s][si] = n;
     task(a,s,si+1,n,k);
     task(a,s,si-1,n,k);
     task(a,s+1,si,n,k);
