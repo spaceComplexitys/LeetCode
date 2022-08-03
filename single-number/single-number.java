4daa5908b10acc2da73265bfc87f1db73f5caddf
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-08-03 18:50:25 -0400

    Time: 1 ms, Memory: 50.5 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,24 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> numberRepeats = new HashMap<>();
-
-        int singleNumber = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (numberRepeats.containsKey(nums[i])) {
-                numberRepeats.put(nums[i], numberRepeats.get(nums[i]) + 1);
-            } else {
-                numberRepeats.put(nums[i], 1);
-            }
-
-        }
-
-        for (Map.Entry<Integer, Integer> entry : numberRepeats.entrySet()) {
-            if (entry.getValue() == 1) {
-                singleNumber = entry.getKey();
-            }
-        }
-
-        return singleNumber;
-    }
+    int ans =0;
+    
+    int len = nums.length;
+    for(int i=0;i!=len;i++)
+        ans ^= nums[i];
+    
+    return ans;
+    
+}
 }
