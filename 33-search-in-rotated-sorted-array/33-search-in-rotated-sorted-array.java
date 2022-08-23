d514fe56facaebaf71c21141981e862bafb98e24
David Kim <89640845+spaceComplexitys@users.noreply.github.com>
2022-08-23 19:36:56 -0400

    Time: 1 ms (76.46%), Space: 42.9 MB (25.66%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,43 @@
+class Solution {
+      public int search(int[] nums, int target) {
+
+
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left <= right) {
+                int mid = ((left + right) / 2);
+
+                if (nums[mid] == target) {
+                    return mid;
+                }
+
+
+                // Check if the left side is sorted
+                if (nums[left] <= nums[mid]) {
+                    if (nums[left] <= target && target <= nums[mid]) {
+                        // target in the left
+                        right = mid - 1;
+
+                    } else {
+                        // target in right
+                        left = mid + 1;
+                    }
+                }
+
+                else {
+                    if (nums[mid] <= target && target <= nums[right]) {
+                        left = mid + 1;
+
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+
+
+            }
+
+            return -1;
+        }
+
+}
